08970552d7153cc8964935146a071c2ac158c68e
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2021-09-20 22:59:56 +0300

    add js

---
+++ src/components/Footer.js
@@ -0,0 +1,5 @@
+function Footer() {
+  return <footer className="App-footer"></footer>;
+}
+
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,5 @@
+function Nav() {
+  return <nav className="App-nav"></nav>;
+}
+
+export default Nav;
